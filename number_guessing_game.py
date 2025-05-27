906a493d70e5a48c6638eb59bea4f1a4fa5d47aa
Alvix.exe <alvixwick@gmail.com>
2025-05-27 06:43:22 -0400

    Added the line of code that allows the user to input the desired option.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,8 +3,10 @@ def number_guessing_game():
     print("Please select the difficulty level:\n" \
           " 1. Easy (10 chances)\n" \
           " 2. Medium (5 chances)\n" \
-          " 3. Hard (3 chances)"
+          " 3. Hard (3 chances)\n"
           )
+    
+    option = input("Enter your choice: ")
 
 print("Welcome to the Number Guessing Game!\n"
       "I'm thinking of a number between 1 and 100.\n"
