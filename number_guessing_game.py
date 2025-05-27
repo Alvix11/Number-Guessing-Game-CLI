16e33b5410211733b2fc4fc11a67271addbc51c7
Alvix.exe <alvix.wick@gmail.com>
2025-05-27 08:13:20 -0400

    Created the structure that could hold the logic for the options.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -8,6 +8,17 @@ def number_guessing_game():
     
     option = input("Enter your choice: ")
     option_validate = validate_number(option)
+
+    if option_validate:
+
+        if option_validate == 1:
+            pass
+        elif option_validate == 2:
+            pass
+        elif option_validate == 3:
+            pass
+        else:
+            print("Option not available.")
     
 
 def convert_number(num):
