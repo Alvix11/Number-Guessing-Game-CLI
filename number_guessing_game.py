a334dc98ef167410c99de950d280f87c8ecea7cb
Alvix.exe <alvixwick@gmail.com>
2025-05-27 06:39:48 -0400

    New print statement that displays the game options to the user: easy, medium, and hard.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,2 +1,14 @@
 def number_guessing_game():
-    pass
+
+    print("Please select the difficulty level:\n" \
+          " 1. Easy (10 chances)\n" \
+          " 2. Medium (5 chances)\n" \
+          " 3. Hard (3 chances)"
+          )
+
+print("Welcome to the Number Guessing Game!\n"
+      "I'm thinking of a number between 1 and 100.\n"
+      "You have 5 chances to guess the correct number.\n"
+      )
+
+number_guessing_game()
