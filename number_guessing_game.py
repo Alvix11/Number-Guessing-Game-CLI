c77deac01a50cb88ecaf1e7ea8f6c4d5072debf9
Alvix.exe <alvix.wick@gmail.com>
2025-06-05 21:58:02 -0400

    Moved the welcome print statement outside the function so it runs only once, instead of every time the user presses 1 to continue playing.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,14 +2,14 @@ import random
 import time
 import sys
 
-def number_guessing_game():
-
-    print("\nWelcome to the Number Guessing Game!\n"
+print("\nWelcome to the Number Guessing Game!\n"
       "I'm thinking of a number between 1 and 100.\n"
-      "You have 5 chances to guess the correct number.\n"
+      "You have 5 chances to guess the correct number."
       )
 
-    print("Please select the difficulty level:\n" \
+def number_guessing_game():
+
+    print("\nPlease select the difficulty level:\n" \
           " 1. Easy (10 chances)\n" \
           " 2. Medium (5 chances)\n" \
           " 3. Hard (3 chances)\n"
