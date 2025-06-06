1e4fd419090541265ef896126f95602d067f33fb
Alvix.exe <alvix.wick@gmail.com>
2025-06-05 22:47:00 -0400

    Created the 'show_difficult_menu()' function to display the menu to the user.

---
+++ utils.py
@@ -0,0 +1,6 @@
+def show_difficult_menu():
+    print("\nPlease select the difficulty level:\n" \
+          " 1. Easy (10 chances)\n" \
+          " 2. Medium (5 chances)\n" \
+          " 3. Hard (3 chances)\n"
+          )
