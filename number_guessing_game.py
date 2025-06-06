d0aa8990fc716de4ea93756c1f09542c7cf63c9b
Alvix.exe <alvix.wick@gmail.com>
2025-06-06 00:03:36 -0400

    Added a docstring to the 'main' function.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,7 +2,7 @@ from utils import show_welcome_message
 from game import number_guessing_game
 
 def main():
-
+    '''Main function that displays the welcome message and starts the game.'''
     show_welcome_message()
     number_guessing_game()
 
